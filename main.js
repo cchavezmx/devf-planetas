95204fe743040dee6982656724a0bdd87a41aaee
cchavezmx <cchavezmx@outlook.com>
2022-11-10 20:24:11 -0600

    clase de promiseall

--- main.js
+++ main.js
@@ -1,23 +1,38 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
+const baseURL = 'https://planets-info-by-newbapi.p.rapidapi.com/api/v1/planet/list'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const getPlanets = async () => {
+  console.log('ejecutado!!!')
 
-setupCounter(document.querySelector('#counter'))
+  const listOfPlanets = new Promise((resolve, reject) => {
+    
+    return fetch(baseURL, {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': '20833e1faemsh1ac4de30e1e813dp14d672jsnfcbb2e631bed',
+        'X-RapidAPI-Host': 'planets-info-by-newbapi.p.rapidapi.com'
+      }
+    })
+    .then(res => res.json())
+    .then(json => {
+      // podemos manejar los erroes con el reject que es un callback
+      // reject(() => 'otra cosa')
+      // throw new Error('hubo un errro')
+      resolve(json)
+    })
+  })
+  .then(res => {
+    // aqui quiero guardar la respuesta en el localstorage a manera de cache
+    // la respuesta de la promesa
+    // redis
+    localStorage.setItem('planetas', JSON.stringify(res))
+    return res
+  })
+  .catch(errores => console.log(errores))
+
+
+  // return listOfPlanets
+  
+}
+
+getPlanets()
+
