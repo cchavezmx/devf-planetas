58ef46eae166e797c1f7932b352701154faf1baf
FerEscobarDev <jeiesc04@outlook.com>
2022-11-19 01:01:02 -0500

    Planets Project Finished

--- main.js
+++ main.js
@@ -1,38 +1,105 @@
-const baseURL = 'https://planets-info-by-newbapi.p.rapidapi.com/api/v1/planet/list'
+import './css/style.css';
+import { getPlanetsAsync } from './js/planets.js';
 
-const getPlanets = async () => {
-  console.log('ejecutado!!!')
+let planets = [];
 
-  const listOfPlanets = new Promise((resolve, reject) => {
+const listPlanets = async () => {
+
+    const planetsLocal = localStorage.getItem('planetsLocal');
+
+    if(planetsLocal)
+    {
+        planets = JSON.parse(planetsLocal);
+    }
+    else
+    {
+        const planetsList = await getPlanetsAsync();
+
+        if(planetsList)
+        {
+            localStorage.setItem('planetsLocal', JSON.stringify(planetsList));
+            planets = planetsList;
+        }        
+    }
     
-    return fetch(baseURL, {
-      method: 'GET',
-      headers: {
-        'X-RapidAPI-Key': '20833e1faemsh1ac4de30e1e813dp14d672jsnfcbb2e631bed',
-        'X-RapidAPI-Host': 'planets-info-by-newbapi.p.rapidapi.com'
-      }
-    })
-    .then(res => res.json())
-    .then(json => {
-      // podemos manejar los erroes con el reject que es un callback
-      // reject(() => 'otra cosa')
-      // throw new Error('hubo un errro')
-      resolve(json)
-    })
-  })
-  .then(res => {
-    // aqui quiero guardar la respuesta en el localstorage a manera de cache
-    // la respuesta de la promesa
-    // redis
-    localStorage.setItem('planetas', JSON.stringify(res))
-    return res
-  })
-  .catch(errores => console.log(errores))
-
-
-  // return listOfPlanets
-  
 }
 
-getPlanets()
+listPlanets();
+
+addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('#menu');
+    const imgContainer = document.querySelector('#img-container');
+    const title = document.querySelector('#title');
+    const descriptionPlanet = document.querySelector('#description');
+    const img = document.createElement('img');
+    img.className = 'img-planet';
+
+
+    planets.forEach( ({name, key, description, imgSrc}) => {
+        const itemNav = document.createElement('a');
+        itemNav.textContent = name;
+        itemNav.setAttribute('data_key', key);
+
+        if(key === 'zmxk1zx92lo8')
+        {
+            itemNav.className = 'active';
+            const [ image ] = imgSrc;
+
+            img.src = image.img;
+            img.alt = image.description;            
+            imgContainer.appendChild(img);
+
+            title.textContent = name;
+            descriptionPlanet.textContent = description;
+        }
+
+        menu.appendChild(itemNav);
+    });
+
+    const items = document.querySelectorAll('#menu a');
+
+    items.forEach( item => {
+        item.addEventListener('click', () => {
+            
+            // Se selecciona el item activo para proceder a desactivarlo
+            const itemActive = document.querySelector('.active');
+            itemActive.classList.toggle('active')
+            
+            // Se optiene la key del item al que se hizo click
+            const keyItem = item.getAttribute('data_key');
+            const planetCurrent = planets.find( ({key}) => key === keyItem);
+            const { imgSrc, name, description } = planetCurrent;
+            const [ image ] = imgSrc;
+
+            img.src = image.img;
+            img.alt = image.description;            
+            imgContainer.appendChild(img);
+
+            title.textContent = name;
+            descriptionPlanet.textContent = description;
+
+
+            item.classList.toggle('active');
+
+
+        });
+    });
+
+});
+
+/* const promesa = new Promise((resolve, reject) => {
+    setTimeout(() =>{
+        //console.log('Se ha ejecutado después de 2 segundos.');
+        //Cuando se resuelve la promesa en necesario llamar al resolve o al reject según sea el caso
+        resolve('perro y gato');
+    }, 2000);
+});
+
+//then => Lo que devuelve la promesa cuando se ejecuta satisfactoriamente
+//cath => Lo que devuelve la promesa cuando ha ocurrido un error
+
+
+promesa.then((perro) => {
+    console.log('La promesa se ha resuelto y se da respuesta usando el then.',perro);
+}); */
 
